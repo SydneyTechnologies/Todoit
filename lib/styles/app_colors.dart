8fca3d8dae1d5ed5fa254c9dc045135fb4aacdea
Durotimi <timi@weprototype.co>
2022-04-26 13:21:58 +0100

    Merged startpage branch into this branch

--- lib/styles/app_colors.dart
+++ lib/styles/app_colors.dart
@@ -1,4 +1,10 @@
 import 'package:flutter/material.dart';
 class AppColors{
-  static const Color kAppColor = Color(0xffE8505B);
+  static const Color kAppColor = Color(0xff1F1D2B);
+  static const Color kButtonColor = Color(0xff6f6fc8);
+  static const Color cardBGColorOne = Color(0xff3A3A4C);
+  static const Color cardBGColorTwo = Color(0xff272636);
+  static const Color allBGColor = Color(0xff1F1D2B);
+  static const Color dateColor = Color(0xff91919A);
+  static const Color descriptionColor = Color(0xff91919A);
 }
